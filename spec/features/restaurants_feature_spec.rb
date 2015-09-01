efd8bdb3f121e94cb016a083c314b83717741f96
Ben <ben.c.hawker@gmail.com>
2015-09-01 11:59:07 +0100

    Step2

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+  	before do
+  		Restaurant.create(name: "KFC")
+  	end
+
+  	scenario 'display restaurants' do
+  		visit '/restaurants'
+  		expect(page).to have_content("KFC")
+  		expect(page).not_to have_content("No restaurants yet")
+  	end
+  end
 end
