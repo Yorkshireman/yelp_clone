7dae0732092b56621fc7e3467a15ada8ebf4b229
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-01 17:27:06 +0100

    write test for testing reviews dependency on restaurant instance

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
   it {is_expected.to have_many :reviews}
+
+  it "when a restaurant is deleted, its reviews are deleted too" do
+    @restaurant = Restaurant.create(name: "random")
+    @restaurant.reviews.create(thoughts: "blah blah", rating: 4)
+    expect { @restaurant.destroy }.to change {Review.count}.by(-1)
+  end
 end
