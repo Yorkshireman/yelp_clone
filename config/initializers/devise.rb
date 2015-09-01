14ed31c58d7f036da7c04ccfb210e2cda2781787
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-01 19:19:20 +0100

    insert omniauth facebook key and secret into devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :facebook, "1488130158173966", "954843b6b228cc5fbbfece8b47309c3f"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
