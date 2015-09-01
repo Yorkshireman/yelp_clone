8108c4137dcbaf41092f0b91734ff23cf15ef479
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-01 18:42:10 +0100

    installed devise and created user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :restaurants do
     resources :reviews
   end
+
+  root to: "restaurants#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
