b8bb45499d44a3ed325b7d3b3e013b3761abcb84
Ben <ben.c.hawker@gmail.com>
2015-09-01 15:08:09 +0100

    Editing finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,13 +3,29 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.new
   end
 
   def create
     Restaurant.create(restaurant_params)
+
     redirect_to restaurants_path
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
   end
 
   def restaurant_params
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -31,4 +31,29 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
   end
+
+  context "viewing restaurants" do
+    let!(:kfc) { Restaurant.create(name:"KFC") }
+
+    scenario "lets a user view a restaurant" do
+      visit "/restaurants"
+      click_link "KFC"
+      expect(page).to have_content("KFC")
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
+
+  context "editing restaurants" do
+    before { Restaurant.create name: "KFC" }
+
+    scenario "let a user edit a restaurant" do
+      visit "/restaurants"
+      click_link "Edit KFC"
+      fill_in "Name", with: "Kentucky Fried Chicken"
+      click_button "Update Restaurant"
+      expect(page).to have_content "Kentucky Fried Chicken"
+      expect(current_path).to eq "/restaurants"
+    end
+  end
+
 end
