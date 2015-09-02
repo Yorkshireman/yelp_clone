93bb495e40001aeec010265f4ea6a899c0516369
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-02 17:54:13 +0100

    one test failing

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Restaurant, type: :model do
   it {is_expected.to have_many :reviews}
 
+  let(:user) { User.create(email: "test@test.com", password: "passwordpass", password_confirmation: "passwordpass") }
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: 'kf')
     expect(restaurant).to have(1).error_on(:name)
@@ -10,15 +12,16 @@ RSpec.describe Restaurant, type: :model do
   end
 
   it 'is not valid unless it has a unique name' do
-    Restaurant.create(name: "Moe's Tavern")
-    restaurant = Restaurant.new(name: "Moe's Tavern")
+    user.restaurants.create(name: "Moe's Tavern")
+    restaurant = user.restaurants.new(name: "Moe's Tavern")
     expect(restaurant).to have(1).error_on(:name)
   end
 
   it "when a restaurant is deleted, its reviews are deleted too" do
-    @restaurant = Restaurant.create(name: "random")
-    @restaurant.reviews.create(thoughts: "blah blah", rating: 4)
-    expect { @restaurant.destroy }.to change {Review.count}.by(-1)
+    user.restaurants.create(name: "restaurant")
+    restaurant = Restaurant.last
+    restaurant.reviews.create(thoughts: "blah blah", rating: 4)
+    expect { restaurant.destroy }.to change {Review.count}.by(-1)
   end
 
   xit "cannot be created without a user_id"
