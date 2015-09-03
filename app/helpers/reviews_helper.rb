6b27f7ef7bde70f50b34b0616bd9ee2be50e32e2
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-03 17:34:34 +0100

    display restaurant average rating as a star rating

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,7 @@
 module ReviewsHelper
+  def star_rating(rating)
+    return rating unless rating.respond_to?(:round)
+    remainder = (5 - rating)
+    "★" * rating.round + "☆" * remainder
+  end
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsHelper, type: :helper do
-  context '#star_rating' do
+  describe '#star_rating' do
     it 'does nothing for not a number' do
       expect(helper.star_rating('N/A')).to eq 'N/A'
+    end
+
+    it 'returns five black stars for 5' do
+      expect(helper.star_rating(5)).to eq '★★★★★'
+    end
+
+    it 'returns 3 black stars and 2 white stars for 3' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
+    it 'returns 4 black stars and 1 white star for 3.5' do
+      expect(helper.star_rating(3.5)).to eq '★★★★☆'
     end
   end
 end
