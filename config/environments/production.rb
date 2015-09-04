fd56b8062fa917ef71eb4448a46e5fb4e65fb98d
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-04 13:19:58 +0100

    fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID']
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
